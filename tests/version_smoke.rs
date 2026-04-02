a3d23ee4127bfa66a6531edcdcb6f2bb3b106769
Maxwell Clarke <maxeonyx@gmail.com>
2026-04-02 17:52:23 +1300

    test: derive version smoke expectation from Cargo metadata

--- tests/version_smoke.rs
+++ tests/version_smoke.rs
@@ -2,11 +2,13 @@ mod common;
 
 use predicates::prelude::*;
 
+const EXPECTED_VERSION_OUTPUT: &str = concat!("oc ", env!("CARGO_PKG_VERSION"), "\n");
+
 #[test]
 fn version_flag_prints_package_version() {
     common::oc_cmd()
         .arg("--version")
         .assert()
         .success()
-        .stdout(predicate::str::contains("oc 0.1.1\n"));
+        .stdout(predicate::str::contains(EXPECTED_VERSION_OUTPUT));
 }
